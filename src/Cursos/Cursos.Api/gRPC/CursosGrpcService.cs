c7bd46abf4b9a4885e5d30fa69a2b5423a334baa
Luis Cuya <luis.cuya@intelica.com>
2025-06-18 11:46:51 -0500

    feat: se agrega el servicio, para esto primero debo tener referenciado el protobug en csproject yel archivo cursos.proto que viene hacer el contrato o interfaz para poder usar el servicio de CursosGrpcService

--- src/Cursos/Cursos.Api/gRPC/CursosGrpcService.cs
+++ src/Cursos/Cursos.Api/gRPC/CursosGrpcService.cs
@@ -1,5 +1,27 @@
-﻿namespace Cursos.Api.gRPC;
+﻿using Cursos.Application.Cursos.ObtenerCurso;
+using Grpc.Core;
+using MediatR;
 
-public class CursosGrpcService
+namespace Cursos.Api.gRPC;
+
+public class CursosGrpcService : CursosService.CursosServiceBase
 {
+    private readonly ISender _sender;
+
+    public CursosGrpcService(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    public override async Task<CursoResponse> CursoExists(CursoRequest request, ServerCallContext context)
+    {
+        var query = new GetCursoQuery(Guid.Parse(request.CursoId));
+        var curso = await _sender.Send(query);
+
+        return new CursoResponse
+        {
+            Exists = curso.IsSuccess,
+        };
+    }
+
 }
